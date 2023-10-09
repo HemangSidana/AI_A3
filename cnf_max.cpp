52df8ed2ca55da465542909b5afff35630da6b86
Rupayan36 <101242054+Rupayan36@users.noreply.github.com>
2023-10-09 23:09:12 +0530

    Add files via upload

--- cnf_max.cpp
+++ cnf_max.cpp
@@ -6,16 +6,19 @@
 #include <unistd.h>
 using namespace std;
 
-vector<int> search(int k, int n, int edg, bool **edges, string file){
+int search(int k, int n, int edg, vector<vector<bool>>&edges, string file , vector<int>&deg){
     string out=file;
     out+=".satinput";
     std::ofstream outputFile(out);
-    if(k>n || k*(k-1)/2 > edg){
+    if(k>n || (k*(k-1)/2) > edg){
         outputFile <<"p cnf "<<1<<" "<<2<<endl;
         outputFile <<-1<<" "<<0<<"\n";
         outputFile <<1<<" "<<0<<"\n";
+        outputFile.close();
         return 0;
     }
+    // vector<int> deg(n+1,0);
+    vector<int> is(n+1,1);
     bool run=true;
     while(run){
         bool change=false;
@@ -107,15 +110,86 @@ vector<int> search(int k, int n, int edg, bool **edges, string file){
         outputFile<<endl;
     }
     outputFile.close();
+
+    std::string command = "minisat ";
+
+    // Replace 'test.satinput' and 'file.satoutput' with your actual file names
+    std::string inputFileName = file +".satinput";
+    std::string outputFileName = file + ".satoutput";
+
+    // Concatenate the file names to the base command
+    command += inputFileName + " " + outputFileName;
+
+    // Convert the command to a C-style string (const char*) for system
+    const char* cCommand = command.c_str();
+
+    // Execute the command using the system function
+    int result = std::system(cCommand);
+
+    //////////////////////
+
+    // command = "./run2.sh " + file;
+    // const char* cCommand1 = command.c_str();
+    // system(cCommand1);
+
+    ////////////////
+
+    fstream inputFile;
+    inputFile.open(outputFileName, ios::in);
+
+    string line;
+    vector<string>inp;
+    while (std::getline(inputFile, line)) {
+    // Process the read line
+        inp.push_back(line);
+    // std::cout << "Read line: " << line << std::endl;
+    }
+    if(inp[0]=="SAT"){
+        string out2=file;
+        out2+=".mapping";
+        std::ofstream outputFile2(out2);
+        std::string input = inp[1];
+        std::istringstream iss(input);
+        std::vector<int> tokens;
+
+        std::string token;
+        while (iss >> token) {
+            tokens.push_back(stoi(token));
+        }
+        outputFile2<<"#1"<<endl;
+        for(int i=0;i<n;i++){
+            if(tokens[i]>0) outputFile2<<tokens[i]<<" ";
+        }
+        outputFile2<<endl;
+        inputFile.close();
+        outputFile2.close();
+        return 1;
+
+    }
+    inputFile.close();
+    return 0;
+
 }
 
 int main(int argc, char* argv[]){
-    string file="";
-    string inputfilename (argv[1]);
-    for(auto x: argv[1]){
-        if(x=='.') break;
-        file+=x;
+    // string file="";
+    // string inputfilename (argv[1]);
+    // for(auto x: inputfilename){
+    //     if(x=='.') break;
+    //     file+=x;
+    // }
+
+    std::string inputfilename(argv[1]);
+    std::string file = "";
+
+    // Find the position of the last period in the inputfilename
+    size_t lastPeriodPos = inputfilename.find_last_of('.');
+
+    if (lastPeriodPos != std::string::npos) {
+        // Extract the part of the inputfilename before the last period
+        file = inputfilename.substr(0, lastPeriodPos);
     }
+
     fstream inputFile;
     inputFile.open(inputfilename, ios::in);
     if (!inputFile) {
@@ -145,10 +219,11 @@ int main(int argc, char* argv[]){
     }
     inputFile.close();
     int n,edg;
-    n=tokens[0]; edg=token[1];
+    n=tokens[0]; edg=tokens[1];
     vector<int> deg(n+1,0);
-    vector<int> is(n+1,1);
-    bool edges[n+1][n+1];
+    // vector<int> is(n+1,1);
+    // bool edges[n+1][n+1];
+    vector<vector<bool>>edges(n+1,vector<bool>(n+1,0));
     for(int i=0;i<=n;i++){for(int j=0;j<=n;j++) edges[i][j]=false;}
 
     for(int i=0;i<edg;i++){
@@ -161,5 +236,18 @@ int main(int argc, char* argv[]){
         edges[u][v]=1;
         edges[v][u]=1;
     }
-    search(k);
+    
+    int hi=n; int lo=1; int ans;
+    while(hi>=lo){
+        int mid=(hi+lo)/2;
+        if(search(mid,n,edg,edges,file,deg)){
+            ans=mid;
+            lo=mid+1;
+        }
+        else{
+            hi=mid-1;
+        }
+    }
+    cout<<ans<<endl;
+
 }
