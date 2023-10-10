cad188186b016586c73d916d052237ec8e8ebfc9
Hemang Sidana <97731299+HemangSidana@users.noreply.github.com>
2023-10-10 13:18:22 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 inp="$1"
-cpp_file="cnf3.cpp"
+cpp_file="cnf.cpp"
 input_file="${inp}.graph"
 output_file="${inp}.satinput"
-./compiled_p1 "$input_file" > "$output_file"
+./c1 "$input_file" > "$output_file"
